71e4baa3147bbd5984cf07dd7a8cd90e0840c7c8
monysary <sary.mony@gmail.com>
2024-09-02 19:27:00 -0700

    updated copy for skills section

--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -25,85 +25,46 @@ function Tool({ title, href, children }) {
 
 export const metadata = {
   title: 'Skills',
-  description: 'Software I use, gadgets I love, and other things I recommend.',
+  description: 'Bring Ideas to Life with the Right Skills & Technologies.',
 }
 
 export default function Skills() {
   return (
     <SimpleLayout
-      title="Software I use, gadgets I love, and other things I recommend."
-      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+      title="Bring Ideas to Life with the Right Skills & Technologies."
+      intro="I combine a solid foundation in both Front End and Back End technologies with a strong set of tools, methodologies, and soft skills.
+      Below are just a few of the key skills and technologies I work with on a day to day basis.
+      My goal is to create high quality digital solutions that meet client needs and exceed expectations."
     >
       <div className="space-y-20">
-        <ToolsSection title="Workstation">
-          <Tool title="16” MacBook Pro, M1 Max, 64GB RAM (2021)">
-            I was using an Intel-based 16” MacBook Pro prior to this and the
-            difference is night and day. I’ve never heard the fans turn on a
-            single time, even under the incredibly heavy loads I put it through
-            with our various launch simulations.
-          </Tool>
-          <Tool title="Apple Pro Display XDR (Standard Glass)">
-            The only display on the market if you want something HiDPI and
-            bigger than 27”. When you’re working at planetary scale, every pixel
-            you can get counts.
-          </Tool>
-          <Tool title="IBM Model M SSK Industrial Keyboard">
-            They don’t make keyboards the way they used to. I buy these any time
-            I see them go up for sale and keep them in storage in case I need
-            parts or need to retire my main.
-          </Tool>
-          <Tool title="Apple Magic Trackpad">
-            Something about all the gestures makes me feel like a wizard with
-            special powers. I really like feeling like a wizard with special
-            powers.
-          </Tool>
-          <Tool title="Herman Miller Aeron Chair">
-            If I’m going to slouch in the worst ergonomic position imaginable
-            all day, I might as well do it in an expensive chair.
+        <ToolsSection title="Front End">
+          <Tool title="HTML, CSS, JavaScript, React, NextJS, Tailwind">
+            I specialize in building responsive, visually appealing interfaces using HTML, CSS, and JavaScript, with a focus on frameworks like React.
+            My work ensures a seamless user experience across all devices.
           </Tool>
         </ToolsSection>
-        <ToolsSection title="Development tools">
-          <Tool title="Sublime Text 4">
-            I don’t care if it’s missing all of the fancy IDE features everyone
-            else relies on, Sublime Text is still the best text editor ever
-            made.
-          </Tool>
-          <Tool title="iTerm2">
-            I’m honestly not even sure what features I get with this that aren’t
-            just part of the macOS Terminal but it’s what I use.
-          </Tool>
-          <Tool title="TablePlus">
-            Great software for working with databases. Has saved me from
-            building about a thousand admin interfaces for my various projects
-            over the years.
+        <ToolsSection title="Back End">
+          <Tool title="Node, Express, MySQL, MongoDB">
+            I develop robust, scalable server-side applications using technologies like Node and Express.
+            My experience includes setting up APIs and managing databases to ensure secure and efficient data handling.
           </Tool>
         </ToolsSection>
-        <ToolsSection title="Design">
-          <Tool title="Figma">
-            We started using Figma as just a design tool but now it’s become our
-            virtual whiteboard for the entire company. Never would have expected
-            the collaboration features to be the real hook.
+        <ToolsSection title="Tools">
+          <Tool title="Git, Figma, Jira">
+            I use Git for version control and collaboration, ensuring projects are managed efficiently and changes are tracked reliably.
+            Figma helps me create clear, visually appealing designs, while Jira supports project management and task organization.
           </Tool>
         </ToolsSection>
-        <ToolsSection title="Productivity">
-          <Tool title="Alfred">
-            It’s not the newest kid on the block but it’s still the fastest. The
-            Sublime Text of the application launcher world.
+        <ToolsSection title="Methodologies">
+          <Tool title="Agile/Scrum, CI/CD">
+            I utilize Agile and Scrum methodologies to promote flexibility and continuous feedback throughout the development process.
+            I also implement CI/CD practices to ensure effective testing and deployment of new features and builds.
           </Tool>
-          <Tool title="Reflect">
-            Using a daily notes system instead of trying to keep things
-            organized by topics has been super powerful for me. And with
-            Reflect, it’s still easy for me to keep all of that stuff
-            discoverable by topic even though all of my writing happens in the
-            daily note.
-          </Tool>
-          <Tool title="SavvyCal">
-            Great tool for scheduling meetings while protecting my calendar and
-            making sure I still have lots of time for deep work during the week.
-          </Tool>
-          <Tool title="Focus">
-            Simple tool for blocking distracting websites when I need to just do
-            the work and get some momentum going.
+        </ToolsSection>
+        <ToolsSection title="Soft SKills">
+          <Tool title="Problem Solving, Communication, Dependable, High work ethic">
+            My background in research has honed my analytical thinking and problem solving abilities.
+            I value clear communication and collaboration, ensuring projects run smoothly and meet client goals.
           </Tool>
         </ToolsSection>
       </div>
